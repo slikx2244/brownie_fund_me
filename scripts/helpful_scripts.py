5f13e5effab99964f69fe4b6df6639708664414e
slikx2244 <slikx2244@gmail.com>
2022-04-20 16:48:49 -0400

    first commit

---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,32 @@
+from brownie import network, accounts, config, MockV3Aggregator
+from web3 import Web3
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+
+# WAS 18 and 2000 before, check vid @5:43:19
+DECIMALS = 8
+STARTING_PRICE = 200000000000
+
+
+def get_account():
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
+
+
+def deploy_mocks():
+    print(f"The active network is {network.show_active()}")
+    print("Deploying Mocks...")
+    if len(MockV3Aggregator) <= 0:
+        MockV3Aggregator.deploy(
+            # remove webtoWei(2000, "ether", check vid @5:43:50)
+            DECIMALS,
+            STARTING_PRICE,
+            {"from": get_account()},
+        )
+    print("Mocks Deployed!")
